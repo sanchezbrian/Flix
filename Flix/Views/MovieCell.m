fe83a9d17a0f4d9087b8eb8b7b28c85afaae1e0a
Brian Sanchez <brian36@fb.com>
2020-06-26 11:27:23 -0700

    initial selection effect

--- Flix/Views/MovieCell.m
+++ Flix/Views/MovieCell.m
@@ -17,8 +17,23 @@
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
+    self.backgroundColor = UIColor.clearColor;
+}
 
-    // Configure the view for the selected state
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    self.backgroundColor = UIColor.grayColor;
+    [super touchesBegan:touches withEvent:event];
+}
+
+-(void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    self.backgroundColor = UIColor.clearColor;
+    [super touchesEnded:touches withEvent:event];
+}
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    self.backgroundColor = UIColor.clearColor;
+    [super touchesCancelled:touches withEvent:event];
 }
 
 @end
